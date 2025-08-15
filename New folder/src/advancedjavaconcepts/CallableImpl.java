70134cd9d6275b028e6adc4da9f17c79e5465669
Tania-Pri <tania.priyadarshani21@gmail.com>
2025-08-15 22:11:14 +0530

    okay

---
+++ New folder/src/advancedjavaconcepts/CallableImpl.java
@@ -0,0 +1,12 @@
+package advancedjavaconcepts;
+import java.util.concurrent.*;
+
+
+public class CallableImpl  implements Callable<Integer>{
+	@Override
+	public Integer call() throws Exception {
+	System.out.println("Inside task");
+	return 1;
+	}
+		
+}
---
+++ New folder/src/advancedjavaconcepts/Multithreading.java
@@ -0,0 +1,17 @@
+package advancedjavaconcepts;
+import java.util.concurrent.*;
+
+public class Multithreading {
+
+	public static void main(String[] args) {
+		CallableImpl im = new CallableImpl();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Future future = executor.submit(im);
+		try {
+		future.get();} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
