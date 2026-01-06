d0b05470e00fce1043fc4fb1d71268865393e196
Tania-Pri <tania.priyadarshani21@gmail.com>
2026-01-06 08:36:21 +0530

    okay

---
+++ New folder/src/com/practisebasics/advancedjavaconcepts/RecursionStringReversal.java
@@ -0,0 +1,20 @@
+package com.practisebasics.advancedjavaconcepts;
+
+public class RecursionStringReversal {
+	
+	public static String reverse(String name, String reverseName) {
+		if(name.isEmpty()) return name;
+		reverseName = reverseName.concat(name);
+		 reverse(name.substring(1), reverseName);
+		// reverseName = reverseName.concat(name);
+		 return reverseName;
+	}
+
+	public static void main(String[] args) {
+		String reverseName = reverse("Tania", "");
+		System.out.println(reverseName);
+		
+
+	}
+
+}
