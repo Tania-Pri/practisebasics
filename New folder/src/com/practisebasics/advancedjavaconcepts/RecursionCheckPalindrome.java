1f5b24cf52517815870d668e16700e6da764caa2
Tania-Pri <tania.priyadarshani21@gmail.com>
2026-01-07 09:41:52 +0530

    07/01/2025

--- New folder/src/com/practisebasics/advancedjavaconcepts/RecursionCheckPalindrome.java
+++ New folder/src/com/practisebasics/advancedjavaconcepts/RecursionCheckPalindrome.java
@@ -1,6 +1,12 @@
 package com.practisebasics.advancedjavaconcepts;
 
 public class RecursionCheckPalindrome {
+	public static String palindrome(String word) {
+		if(word.isEmpty())
+			return word;
+		String newWord = palindrome(word.substring(1))+word.charAt(0);
+		return word;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
